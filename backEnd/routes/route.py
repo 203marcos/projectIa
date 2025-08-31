4efc9c0bc5a6b9d59667670ab1dca63afe0f9701
Marcos Dias <marcosaurelioce@hotmail.com>
2025-08-31 20:27:42 -0300

    Update CORS middleware to allow requests from all origins

--- backEnd/routes/route.py
+++ backEnd/routes/route.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # ou ["*"] para liberar para todos
+    allow_origins=["*"],  # ou ["*"] para liberar para todos
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
